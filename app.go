c0f394c8385da8bb4a59b9221297eeaec44769e9
Babenko <babenkosemen@gmail.com>
2017-07-03 00:29:42 +0300

    Minor fix

--- app.go
+++ app.go
@@ -26,13 +26,13 @@ func main() {
 }
 
 func applyArgs() {
-	log.Print(os.Args[0] + " | " + os.Args[1] + " | " + strconv.Itoa(len(os.Args)))
-	if len(os.Args) == 2 && os.Args[1] != "" {
+
+	if len(os.Args) >= 2 && os.Args[1] != "" {
 		service.STORAGE_DIRECTORY = os.Args[1]
 	}
 	log.Print("Files store at " + service.STORAGE_DIRECTORY)
 
-	if len(os.Args) == 3 && os.Args[2] != "" {
+	if len(os.Args) >= 3 && os.Args[2] != "" {
 		size, err := strconv.ParseInt(os.Args[2], 10, 0)
 		if err != nil {
 			log.Panic(err)
@@ -43,12 +43,12 @@ func applyArgs() {
 
 	log.Print("Max file size is " + strconv.FormatInt(service.MAX_FILE_SIZE / 1000000, 10) + " mb.")
 
-	if len(os.Args) == 4 && os.Args[3] != "" {
+	if len(os.Args) >= 4 && os.Args[3] != "" {
 		PORT = os.Args[3]
 	}
 	log.Print("Listen on port " + PORT)
 
-	if len(os.Args) == 5 && os.Args[4] != "" {
+	if len(os.Args) >= 5 && os.Args[4] != "" {
 		service.PARAM_NAME = os.Args[4]
 	}
 	log.Print("Param name is " + service.PARAM_NAME)
