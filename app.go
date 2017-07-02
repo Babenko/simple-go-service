0151b6023ead9f0df739d2e458bd588e73c2029c
Babenko <babenkosemen@gmail.com>
2017-07-02 20:59:17 +0300

    Minor changes

--- app.go
+++ app.go
@@ -7,7 +7,10 @@ import (
 	"net/http"
 	"github.com/babenko/simple-go-service/service"
 	"os"
+	"strconv"
 )
+
+var PORT = "8080"
 
 func main() {
 	router := mux.NewRouter()
@@ -18,10 +21,28 @@ func main() {
 		Methods("POST").
 		HandlerFunc(service.UploadFile)
 	fmt.Println("Starting")
-	log.Fatal(http.ListenAndServe(":8090", router))
+	log.Fatal(http.ListenAndServe(PORT, router))
 }
 
 func applyArgs() {
-	port := os.Args[0]
+	if os.Args[0] != "" {
+		service.STORAGE_DIRECTORY = os.Args[1]
+	}
+	log.Print("Files store at " + service.STORAGE_DIRECTORY)
 
+	if os.Args[1] != "" {
+		size, err := strconv.ParseInt(os.Args[1], 10, 0)
+		if err != nil {
+			log.Panic(err)
+		} else {
+			service.MAX_FILE_SIZE = size
+		}
+	}
+
+	log.Print("Max file size is " + strconv.FormatInt(service.MAX_FILE_SIZE / 1000000, 10) + " mb.")
+
+	if os.Args[2] != "" {
+		PORT = os.Args[0]
+	}
+	log.Print("Listen on port " + PORT)
 }
